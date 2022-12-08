11893a7afae55d86413f5a1c49b5e06081f5f213
Glauber Peterson <Petersonglauber@hotmail.com>
2022-12-08 00:06:02 -0300

    Criando projeto Rooms POO

---
+++ src/quartos.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rooms;
+
+public class quartos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method s
+     
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int n = sc.nextInt();
+		Rooms[] vect = new Rooms [n];
+		
+		for (int i=0;i<vect.length;i++);{
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
